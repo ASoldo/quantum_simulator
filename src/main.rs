322f219ff3d5686fbdcad4de5fb0dff15b98f89e
Rootster <soldo.andrej@gmail.com>
2024-07-29 02:22:30 +0200

    Minor update

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 use quantum_simulator::circuit::Circuit;
-use quantum_simulator::gates::{hadamard, pauli_x};
+use quantum_simulator::gates::{hadamard, pauli_x, pauli_y, pauli_z, phase};
 use quantum_simulator::simulator::Simulator;
 
 fn main() {
     let mut circuit = Circuit::new();
     circuit.add_gate(hadamard());
     circuit.add_gate(pauli_x());
+    circuit.add_gate(pauli_y());
+    circuit.add_gate(pauli_z());
+    circuit.add_gate(phase(1.0));
 
     let initial_state = [1.0, 0.0]; // |0> state
     let final_qubit = Simulator::run(&circuit, initial_state);
