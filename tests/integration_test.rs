456f3482b2e4b165f0cd0291394701bd7e7e3846
Rootster <soldo.andrej@gmail.com>
2024-07-29 02:30:48 +0200

    Update: Add new test -> test_phase()

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -2,7 +2,7 @@
 mod tests {
     use num_complex::Complex;
     use quantum_simulator::circuit::Circuit;
-    use quantum_simulator::gates::{hadamard, pauli_x, pauli_y, pauli_z};
+    use quantum_simulator::gates::{hadamard, pauli_x, pauli_y, pauli_z, phase};
     use quantum_simulator::simulator::Simulator;
 
     #[test]
@@ -40,5 +40,20 @@ mod tests {
 
         assert_eq!(final_qubit.state[0], Complex::new(1.0, 0.0));
         assert_eq!(final_qubit.state[1], Complex::new(0.0, 0.0));
+    }
+    #[test]
+    fn test_phase() {
+        let mut circuit = Circuit::new();
+        circuit.add_gate(phase(std::f64::consts::PI / 2.0));
+
+        let initial_state = [0.0, 1.0]; // |1⟩ state
+        let final_qubit = Simulator::run(&circuit, initial_state);
+
+        // The phase gate with θ = π/2 adds a 90-degree phase shift to the |1⟩ component.
+        assert_eq!(final_qubit.state[0], Complex::new(0.0, 0.0));
+        assert_eq!(
+            final_qubit.state[1],
+            Complex::new(6.123233995736766e-17, 1.0)
+        );
     }
 }
